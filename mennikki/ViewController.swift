230d8c2c6f1aa44120d2e9ea4de24236311b92dc
yagi-shi <yukisoccer.kk@gmail.com>
2026-02-06 23:37:08 +0900

    first commit

---
+++ mennikki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mennikki
+//
+//  Created by 八木佑樹 on 2026/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
